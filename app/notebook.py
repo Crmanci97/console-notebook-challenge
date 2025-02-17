a1db49d4de06c8a924f2ac25d5bc9b0095e070da
Crmanci97 <143139331+Crmanci97@users.noreply.github.com>
2025-02-17 13:28:11 -0500

    def tag   and  def and return  str

--- app/notebook.py
+++ app/notebook.py
@@ -7,7 +7,15 @@ class Note :
     LOW : str = "LOW"
 
     def __init__(self, code : str, title : str, text : str, importance : str):
-        self.code = code
-        self.title = title
-        self.text = text
-        self.importance = importance
+        self.code :str = code
+        self.title : str = title
+        self.text : str = text
+        self.importance : str = importance
+
+    def add_tag(self, tag: str):
+        if tag not in self.tags:
+            self.tags.append(tag)
+            return tag
+
+    def __str__(self):
+        return f"Date: {self.creation_date}\n{self.title}: {self.text}"
