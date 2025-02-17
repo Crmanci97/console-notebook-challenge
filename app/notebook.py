ef04aa651b9b2b6aaf1fba7320fac6baaa5133f9
Crmanci97 <143139331+Crmanci97@users.noreply.github.com>
2025-02-17 12:53:58 -0500

    CLASS NOTE

--- app/notebook.py
+++ app/notebook.py
@@ -1,1 +1,7 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+import datetime
+
+class note :
+    HIGH : str : "HIGH"
+    MEDIUM : STR : "MEDIUM"
+    LOW : str : "LOW"
