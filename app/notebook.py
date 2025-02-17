5c97ea0f8a490363f4b504a1113a967d125f84d3
Crmanci97 <143139331+Crmanci97@users.noreply.github.com>
2025-02-17 13:40:47 -0500

    Filtramos las notas que tienen importancia Filtramos las notas que contienen la etiqueta Contamos cuántas veces aparece cada etiqueta validaciones pdte

--- app/notebook.py
+++ app/notebook.py
@@ -17,5 +17,37 @@ class Note :
             self.tags.append(tag)
             return tag
 
+
     def __str__(self):
         return f"Date: {self.creation_date}\n{self.title}: {self.text}"
+
+    class Notebook:
+        def __init__(self):
+            self.notes = []
+
+    def add_note(self, title: str, text: str, importance: str, tags: list[str]) -> int:
+        code = len(self.notes) + 1
+        new_note = Note(title, text, importance, code, tags)
+        self.notes.append(new_note)
+        return code
+
+    def delete_note(self, code: int):
+        self.notes = [note for note in self.notes if note.code != code]
+
+    def important_notes(self) -> list[Note]:
+        return [note for note in self.notes if note.importance in ['HIGH', 'MEDIUM']]
+
+    def notes_by_tag(self, tag: str) -> list[Note]:
+        return [note for note in self.notes if tag in note.tags]
+
+    def tag_with_most_notes(self) -> str:
+        tag_counts = {}
+        for note in self.notes:
+            for tag in note.tags:
+                tag_counts[tag] = tag_counts.get(tag, 0) + 1
+
+        if not tag_counts:
+            return ""
+
+        # most_common_tag = min(tag_counts.items(), key=lambda item: (-item[1], item[0]))
+        #return most_common_tag[0]
