95852d23769f712d380e9702f46537612336f363
Crmanci97 <143139331+Crmanci97@users.noreply.github.com>
2025-02-17 13:13:23 -0500

    definition clases    and    import data time

--- app/notebook.py
+++ app/notebook.py
@@ -1,7 +1,13 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
 import datetime
 
-class note :
-    HIGH : str : "HIGH"
-    MEDIUM : STR : "MEDIUM"
-    LOW : str : "LOW"
+class Note :
+    HIGH : str = "HIGH"
+    MEDIUM : str = "MEDIUM"
+    LOW : str = "LOW"
+
+    def __init__(self, code : str, title : str, text : str, importance : str):
+        self.code = code
+        self.title = title
+        self.text = text
+        self.importance = importance
